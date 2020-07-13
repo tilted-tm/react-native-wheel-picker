0a84503d4a7eb290161085dd3d0f4cc3c97bd972
Talal MOHAMMAD <talal.m@hotmail.fr>
2020-07-13 18:58:53 +0200

    [CLEAN] - Clean quickly and remove unused things

--- WheelCurvedPicker.android.js
+++ WheelCurvedPicker.android.js
@@ -1,43 +1,44 @@
-'use strict';
+'use strict'
 
-import React from 'react';
-import PropTypes from 'prop-types'
+import * as React from 'react'
 import {
-	View,
 	ColorPropType,
 	requireNativeComponent,
-} from 'react-native';
+	View,
+} from 'react-native'
 
-const defaultItemStyle = { color: 'white', fontSize: 26 };
+const WheelCurvedPickerNative = requireNativeComponent('WheelCurvedPicker');
 
-const WheelCurvedPickerNativeInterface = {
+type Props = {
 	name: 'WheelCurvedPicker',
 	propTypes: {
 		...View.propTypes,
-		data:PropTypes.array,
+		data: Array,
 		textColor: ColorPropType,
-		textSize: PropTypes.number,
-		itemStyle: PropTypes.object,
-		itemSpace: PropTypes.number,
-		onValueChange: PropTypes.func,
-		selectedValue: PropTypes.any,
-		selectedIndex: PropTypes.number,
-		lineColor: PropTypes.any,
-		lineGradientColorFrom: PropTypes.any,
-		lineGradientColorTo: PropTypes.any,
+		textSize: number,
+		itemStyle: React.style,
+		itemSpace: number,
+		onValueChange: () => void,
+		selectedValue: any,
+		selectedIndex: Number,
+		lineColor: any,
+		lineGradientColorFrom: any,
+		lineGradientColorTo: any,
 	}
 }
 
-const WheelCurvedPickerNative = requireNativeComponent('WheelCurvedPicker', WheelCurvedPickerNativeInterface);
 
-class WheelCurvedPicker extends React.Component {
+class WheelCurvedPicker extends React.PureComponent<Props> {
 	constructor(props){
 		super(props)
 		this.state = this._stateFromProps(props)
 	}
 
 	static defaultProps = {
-		itemStyle : {color:"white", fontSize:26},
+		itemStyle : {
+			color:'white',
+			fontSize:26
+		},
 		itemSpace: 20
 	}
 
@@ -46,8 +47,8 @@ class WheelCurvedPicker extends React.Component {
 	}
 
 	_stateFromProps (props) {
-		var selectedIndex = 0;
-		var items = [];
+		let selectedIndex = 0;
+		const items = [];
 		React.Children.forEach(props.children, function (child, index) {
 			if (child.props.value === props.selectedValue) {
 				selectedIndex = index;
@@ -55,56 +56,40 @@ class WheelCurvedPicker extends React.Component {
 			items.push({value: child.props.value, label: child.props.label});
 		});
 
-		var textSize = props.itemStyle.fontSize
-		var textColor = props.itemStyle.color
+		const textSize = props.itemStyle.fontSize
+		const  textColor = props.itemStyle.color
 
 		return {selectedIndex, items, textSize, textColor};
 	}
 
 	_onValueChange = (e) => {
-		if (this.props.onValueChange) {
+		const { onValueChange } = this.props
+		if (onValueChange) {
 			this.props.onValueChange(e.nativeEvent.data);
 		}
 	}
 
 	render() {
-		return <WheelCurvedPickerNative
+		const { items, selectedIndex, textColor, textSize } = this.state
+		return (
+			<WheelCurvedPickerNative
 				{...this.props}
 				onValueChange={this._onValueChange}
-				data={this.state.items}
-				textColor={this.state.textColor}
-				textSize={this.state.textSize}
-				selectedIndex={parseInt(this.state.selectedIndex)} />;
+				data={items}
+				textColor={textColor}
+				textSize={textSize}
+				selectedIndex={parseInt(selectedIndex)} />
+		)
 	}
 }
 
-class Item extends React.Component {
+class Item extends React.PureComponent {
 	render () {
 		// These items don't get rendered directly.
 		return null;
 	}
 }
-		
-WheelCurvedPicker.propTypes = {
-	...View.propTypes,
-	data: PropTypes.array,
-	textColor: ColorPropType,
-	textSize: PropTypes.number,
-	itemStyle: PropTypes.object,
-	itemSpace: PropTypes.number,
-	onValueChange: PropTypes.func,
-	selectedValue: PropTypes.any,
-	selectedIndex: PropTypes.number,
-	lineColor: PropTypes.any,
-	lineGradientColorFrom: PropTypes.any,
-	lineGradientColorTo: PropTypes.any
-}
 
-Item.propTypes = {
-	value: PropTypes.any, // string or integer basically
-	label: PropTypes.string,
-}
+WheelCurvedPicker.Item = Item
 
-WheelCurvedPicker.Item = Item;
-
-module.exports = WheelCurvedPicker;
+export default WheelCurvedPicker
